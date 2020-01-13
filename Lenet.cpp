e3bba6b09035012dfbec2c9d299299bcb045ed47
jefffang19 <jefffang07@gmail.com>
2020-01-14 01:44:36 +0800

    pooling 2 finished

--- Lenet.cpp
+++ Lenet.cpp
@@ -32,8 +32,10 @@ static int total_op_counter = 0;
 
 //use for convolution
 static int filter_cnt1 = 1; //count first conv layer
-static int filter_cnt2 = 1; //count second conv layer
+static int filter_cnt2 = 0; //count second conv layer
+static int depth_cnt = 0;
 static bool start_conv = false;
+static bool start_conv2 = false;
 static int start_of_conv = 0; //starting position of convolution
 static TYPE sum = 0;
 static int kernal_len = sqrt(KERNAL_SIZE);
@@ -42,6 +44,7 @@ static int count_slide = 0;
 
 //pool
 static bool start_pooling = false;
+static bool start_pooling2 = false;
 static TYPE max_pool_value;
 
 
@@ -82,11 +85,12 @@ void lenet::run_lenet(){
         
         //read 1 piece of kernal and 1 piece of img. (after image fully store to ram)
         if(ram_tracker<IMG_SIZE && image_read) img_read_from_ram();
+        //else if(start_conv2 && image_read) img_read_from_ram();
 
-        if((rom_tracker - filter_start) < KERNAL_SIZE +2 && image_read && !start_pooling) read_kernal();
+        if((rom_tracker - filter_start) < KERNAL_SIZE +1 && image_read && !start_pooling && !start_conv2) read_kernal();
         
         //start doing convolution
-        if(start_conv && !dedicate_to_storing){
+        if(start_conv && !start_conv2 && !dedicate_to_storing){
             if(op_cnt++ < KERNAL_SIZE){ //convolution operation
                 sum += img * kernal;
                 //cout << sum << " "; //debug
@@ -146,8 +150,106 @@ void lenet::run_lenet(){
             }
             else if(!dedicate_to_storing) ++count_slide;
         }
+        else if(start_conv2 && !dedicate_to_storing){
+            if(op_cnt++ < KERNAL_SIZE){ //convolution operation
+                img = ram_data_in;
+                kernal = rom_data_in;
+                sum += img * kernal;
+                //cout << sum << " "; //debug
+                //cout << "conv cnt:" << op_cnt << " rom tracker: " << rom_tracker << " ram tracker: " << ram_tracker << " " << " " << img << "\t" << kernal << endl; //debug
+                //cout << kernal << " "; //debug
+            }
+            if(depth_cnt < 6 && op_cnt == KERNAL_SIZE){
+               //clear buffers
+                op_cnt = 0;
+                count_slide = 0;
+
+                //calculate the next starting point
+                start_of_conv += 144;
+
+                //change the start of the filter
+                filter_start += 25;
+                rom_tracker = filter_start;
+
+                //set the next starting point
+                ram_tracker = start_of_conv - 1;
+
+                //depth deeper
+                ++depth_cnt; 
+            }
+            else if(op_cnt == KERNAL_SIZE){ //add bias
+                //debug
+                //cout << "debug: enter 6 stage.\n";
+                sum += kernal;
+
+                //activation function
+                sum = relu(sum);
+
+                ++total_op_counter;
+
+                //if(sum!=0) cout << "debug:" << sum << " " << result_to_ram << " " << total_op_counter << endl;
+
+                //store to ram
+                data_store_to_ram(sum);
+
+                //cout << "Finish: " << sum << endl << endl; //debug
+                //image_read = false; //debug
+                //start_conv = false; //deubg
+                //debug
+
+                if(total_op_counter<64){
+                    //change the start of the filter
+                    filter_start -= 125;
+                    rom_tracker = filter_start;
+                }
+                else if(total_op_counter==64){
+                    start_conv = false;
+
+                    //change the start of the filter
+                    filter_start += 26;
+                    rom_tracker = filter_start;
+
+                    //ram debug setting
+                    //ram_debuging = true;
+                    //img_len = 64;
+                    //kernal_len = 8;
+                }
+                //cout << "DEubg: filterstart: " << filter_start << endl;
+
+                //clear buffers
+                op_cnt = 0;
+                sum = 0;
+                count_slide = 0;
+                //reset kernal depth
+                depth_cnt = 1;
+
+                //calculate the next starting point
+                start_of_conv = start_of_conv - 720 + 1;
+
+                //check if reach image's edge - filter length
+                if((start_of_conv-begin_of_conv_in_ram) % img_len == img_len - (kernal_len - 1)) start_of_conv+=(kernal_len - 1);
+
+                //set the next starting point
+                ram_tracker = start_of_conv - 1;
+            }
+            else{
+                read_kernal();
+            }
+
+            ++ram_tracker;
+
+            //if reach the filter's length
+            if(count_slide == kernal_len -1){
+                ram_tracker = ram_tracker - kernal_len + img_len;
+                //cout << "debug_c:" << ram_tracker << " "; //debug
+                count_slide = 0;
+            }
+            else if(!dedicate_to_storing) ++count_slide;
+
+            if(!dedicate_to_storing) ram_addr = ram_tracker;
+        }
         //max pooling
-        else if(start_pooling && !dedicate_to_storing){
+        else if(start_pooling && !depth_cnt && !dedicate_to_storing){
             if(total_op_counter==0) debug_register();
             img = ram_data_in;
             //first compare -> set max_value
@@ -182,13 +284,18 @@ void lenet::run_lenet(){
             if(total_op_counter==3455){
                 cout << "pooling is done. (debug)\n";
                 start_pooling = false;
-                start_conv = true;
+                start_conv2 = true;
+
+                //move ram pointer back to start of the pooling layer outputs
+                //ram_tracker = begin_of_pool_in_ram;
+                begin_of_conv_in_ram = begin_of_pool_in_ram;
+                begin_of_pool_in_ram = result_to_ram;
 
                 //debugging
-                debug_register();
-                img_len = 12*12*6;
-                kernal_len = 12;
-                ram_debuging = true;
+                //debug_register();
+                //img_len = 12*12*6;
+                //kernal_len = 12;
+                //ram_debuging = true;
             }
             else if(total_op_counter%48==47){
                 ram_tracker+=24;
@@ -198,13 +305,64 @@ void lenet::run_lenet(){
             //debug
             //if(max_pool_value) cout << max_pool_value << " ";
         }
+        //pooling phase 2
+        else if(start_pooling2 && !dedicate_to_storing){
+            img = ram_data_in;
+            //first compare -> set max_value
+            if(op_cnt == 0){
+                max_pool_value = img;
+                ++op_cnt;
+                ++ram_tracker;
+                ram_addr = ram_tracker;
+            }
+            //compare
+            else if(op_cnt < 3){
+                if(max_pool_value<img) max_pool_value = img; 
+                ++op_cnt;
+                
+                //move pointer
+                if(op_cnt%2 == 0) ++ram_tracker;
+                else ram_tracker = ram_tracker +7;
+
+                ram_addr = ram_tracker;
+            }
+            //the 4th compare
+            //store result_to_ram
+            else{
+                if(max_pool_value < img) data_store_to_ram(img);
+                else data_store_to_ram(max_pool_value);
+                op_cnt = 0;
+
+                //move pointer
+                ram_tracker = ram_tracker - 7;
+            }
+            
+            if(total_op_counter==1024){
+                cout << "pooling 2 is done. (debug)\n";
+
+                start_pooling2 = false;
+
+                //move ram pointer back to start of the pooling layer outputs
+                //ram_tracker = begin_of_pool_in_ram;
+                begin_of_conv_in_ram = result_to_ram;
+
+                //debugging
+                debug_register();
+                img_len = 256;
+                kernal_len = 4;
+                ram_debuging = true;
+            }
+            else if(total_op_counter%16==15){
+                ram_tracker+=8;
+            }
+            ++total_op_counter;
+        }
         else if(dedicate_to_storing){
             dedicate_to_storing = false;
             ram_wr = true; //read mode on
 
             //check if done 6 times
             if(!start_conv && filter_cnt1 < CONV_LAYER1){
-                //move the 
                 //move ram pointer back to img (0)
                 ram_tracker = 0;
                 //cout << "debug---\n";
@@ -212,7 +370,7 @@ void lenet::run_lenet(){
                 image_read = true;
                 start_of_conv = 0;
                 //change the start of the filter
-                filter_start += 27;
+                filter_start += 26;
                 rom_tracker = filter_start;
                 //reset counter
                 total_op_counter=0;
@@ -222,9 +380,32 @@ void lenet::run_lenet(){
                 //debug
                 debug_register();
             }
+            //do convolution layer 2
+            else if(start_conv2 && !start_conv && filter_cnt2 < CONV_LAYER2){
+                start_of_conv = begin_of_conv_in_ram;
+                ram_tracker = start_of_conv;
+                ram_addr = ram_tracker;
+
+                if(!ram_debuging) img_len = 12;
+                request_to_rom = false; //let read_kernal signals conv
+
+                read_kernal();
+                
+                //reset counter
+                total_op_counter=0;
+                op_cnt = 0;
+                depth_cnt = 1;
+                sum = 0;
+
+                ++filter_cnt2;
+
+                cout << "DEBUG conv2: " << filter_cnt2 << endl;
+                debug_register();
+            }
             //do max pooling
-            else if(!start_conv && !start_pooling){
-                rom_tracker+=27; //add up the last kernal
+            else if(!start_conv && !start_pooling && !start_conv2 && filter_cnt2 < CONV_LAYER2){
+                //rom_tracker+=26; //add up the last kernal
+                filter_start=rom_tracker;
                 
                 start_pooling = true;
                 ram_tracker = begin_of_conv_in_ram;
@@ -232,14 +413,27 @@ void lenet::run_lenet(){
                 ram_addr = ram_tracker;
                 total_op_counter = 0;
             }
-            //do convolution layer 2
-            else if(start_conv && filter_cnt1 >= CONV_LAYER1 && filter_cnt2 < CONV_LAYER2){
+            //max pooling phase 2
+            else if(!start_conv && !start_pooling2 && start_conv2 && filter_cnt2 == CONV_LAYER2){
+                //start_pooling = true;
+                start_conv2 = false;
+
+                start_pooling2 = true;
                 
-                ++filter_cnt2;
+                op_cnt = 0;
+
+                ram_tracker = begin_of_pool_in_ram;
+                begin_of_conv_in_ram = result_to_ram;
+                total_op_counter = 0;
+
+                cout << "DEBUG conv2: end" << endl;
+                debug_register();
             }
 
+
             //neccessary at max pooling phase
-            if(start_pooling) ram_addr = ram_tracker;
+            if(start_pooling || start_pooling2) ram_addr = ram_tracker;
+
         }
     }
 
@@ -265,6 +459,7 @@ void lenet::run_lenet(){
             counting = 0;
             ff = 0;
             cout << endl << endl;
+            //img_len = 12;
         } 
     }
 
@@ -325,7 +520,7 @@ void lenet::read_kernal(){
         //kernal[rom_tracker++] = rom_data_in;
         kernal = rom_data_in;
         start_conv = true; //signal conv to start adding up
-        //cout << kernal << " ";
+        //cout << "Debug: Kernal " << kernal << " ";
         rom_addr = rom_tracker++;
         if( (rom_tracker - filter_start) == KERNAL_SIZE+2 ) rom_tracker = filter_start;
     }
